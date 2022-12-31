88e4e4de69f3f0ca687d1e4ecdc2bc87a5116e8a
kisung <kisung1037@gmail.com>
2022-12-31 21:00:40 +0900

    ..

--- stage/data-stores/mysql/main.tf
+++ stage/data-stores/mysql/main.tf
@@ -7,11 +7,11 @@ resource "aws_db_instance" "example" {
     engine            = "mysql"
     allocated_storage = 10
     instance_class    = "db.t2.micro"
-    name              = "example_database"
+    db_name           = "example_database"
     username          = "admin"
 
     password = data.aws_secretsmanager_secret_version.db_password.secret_string
 }
-    data "ws_secretsmanager_secret_version" "db_password" {
+data "aws_secretsmanager_secret_version" "db_password" {
         secret_id = "mysql-master-password-stage"
 }
