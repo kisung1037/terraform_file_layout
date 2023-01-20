0428e677f3dc1191245f014556c52f770c4b6f49
kisung <kisung1037@gmail.com>
2023-01-20 09:41:05 +0900

    .

--- live/global/iam/existiong-iam-user/main.tf
+++ live/global/iam/existiong-iam-user/main.tf
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 provider "aws" {
     region = "us-east-2"
 }
@@ -19,26 +18,4 @@ resource "aws_iam_user" "example" {
 
 output "all_users" {
     value = values(aws_iam_user.example)[*].arn
-=======
-provider "aws" {
-    region = "us-east-2"
-}
-
-# resource "aws_iam_user" "example" {
-#     count = length(var.user_names)
-#     name = var.user_names[count.index]
-# }
-
-# output "all_arns" {
-#     value = aws_iam_user.example[*].arn
-# }
-
-resource "aws_iam_user" "example" {
-    for_each = toset(var.user_names)
-    name = each.value
-}
-
-output "all_users" {
-    value = values(aws_iam_user.example)[*].arn
->>>>>>> d0b4abce6cc41cdded5f03229212caa1df306ac0
 }
--- live/global/iam/three-iam-users-unique-names/variables.tf
+++ live/global/iam/three-iam-users-unique-names/variables.tf
@@ -1,12 +1,5 @@
-<<<<<<< HEAD
 variable "user_names" {
     description = "Create IAM users with these names"
     type        = list(string)
     default     = [ "neo","trinity", "morpheus" ]
-=======
-variable "user_names" {
-    description = "Create IAM users with these names"
-    type        = list(string)
-    default     = [ "neo","trinity", "morpheus" ]
->>>>>>> d0b4abce6cc41cdded5f03229212caa1df306ac0
 }
